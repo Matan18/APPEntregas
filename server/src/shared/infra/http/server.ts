83ddbc7f2365eab429bcad0e042e223cdffcca95
Matan18 <mateusandriola18@gmail.com>
2020-08-14 16:52:00 -0300

    Initiate connection before app

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -1,6 +1,8 @@
 import "dotenv/config"
+import createConnection from "../typeorm/database/connections"
 import app from "./routes/control";
+createConnection()
 
-app.listen(3333, ()=>{
-    console.log("[SERVER] Running at port 3333")
+app.listen(3333, () => {
+  console.log("[SERVER] Running at port 3333")
 })
